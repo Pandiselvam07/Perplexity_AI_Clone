0733e0e59813ecb1f72f9218c86605b78013717f
Pandiselvam07 <rpandiselvam07@gmail.com>
2025-08-09 20:07:59 +0530

    Server

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_ai_clone/theme/colors.dart';
+import 'package:perplexity_ai_clone/widgets/search_section.dart';
+import 'package:perplexity_ai_clone/widgets/side_nav_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SideNavBar(),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(child: SearchSection()),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          'Pro',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          'Enterprise',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          'Blog',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          'Store',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          'English(IN)',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/search_section.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:perplexity_ai_clone/theme/colors.dart';
+import 'package:perplexity_ai_clone/widgets/search_bar_button.dart';
+
+class SearchSection extends StatelessWidget {
+  const SearchSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'What do you want to learn today?',
+          style: GoogleFonts.ibmPlexMono(
+            fontSize: 35,
+            fontWeight: FontWeight.w400,
+            height: 1.2,
+            letterSpacing: -1,
+          ),
+        ),
+        SizedBox(height: 30),
+        Container(
+          width: 700,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: AppColors.searchBarBorder, width: 1.5),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'Search anything....',
+                    hintStyle: TextStyle(
+                      color: AppColors.textGrey,
+                      fontSize: 16,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    SearchBarButton(
+                      title: 'Focus',
+                      icon: Icons.auto_awesome_outlined,
+                    ),
+                    SizedBox(width: 15),
+                    SearchBarButton(
+                      title: 'Attach',
+                      icon: Icons.add_circle_outline_rounded,
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: AppColors.submitButton,
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: AppColors.background,
+                        size: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
